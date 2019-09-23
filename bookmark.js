86ae799f80cd16e11de71ccd8daaa0eca937360d
Alan Turing <austeyd@gmail.com>
2019-09-22 22:09:13 -0600

    Add stylings for left section

---
+++ bookmark.js
@@ -0,0 +1,12 @@
+class Bookmark {
+  constructor() {
+    this.id;
+    this.title;
+    this.url;
+    this.read = false
+  }
+  toggleRead() {
+    // This method should be invoked when someone changes whether a Bookmark has been read or not
+    // This method should update the read property on the Bookmark
+  }
+}
